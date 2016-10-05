c1151ef7cb22b22e3a26e884a68f65ae498a3309
Andrew Tran <ajtran@umich.edu>
2016-10-05 11:56:26 -0700

    got rid of repetive code

--- test_rpn.py
+++ test_rpn.py
@@ -5,3 +5,18 @@ class TestBasics(unittest.TestCase):
 	def test_add(self):
 		result = rpn.calculate("1 1 +")
 		self.assertEqual(2, result)
+
+	def test_sub(self):
+		result = rpn.calculate("5 3 -")
+		self.assertEqual(2, result)
+
+	def test_badstring(self):
+		with self.assertRaises(TypeError):
+			rpn.calculate("1 2 3 +")
+	
+	def test_mult(self):
+		result = rpn.calculate("4 3 *")
+		self.assertEqual(12, result)
+	def test_divide(self):
+		result = rpn.calculate("14 7 /")
+		self.assertEqual(2, result)
